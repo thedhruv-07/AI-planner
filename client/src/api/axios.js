c17f576986cd3d71229281822fd9642faf1837bf
dhruv kumar <dhruvsingh200420@gmail.com>
2026-03-09 14:08:48 +0530

    Updated frontend API baseURL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-planner-4xv2.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
