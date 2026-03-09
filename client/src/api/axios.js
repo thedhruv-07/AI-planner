adfe151a344f0788630f3824cb94c659015b6fbb
dhruv kumar <dhruvsingh200420@gmail.com>
2026-03-09 14:25:24 +0530

    Fixed production API base URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-planner-4xv2.onrender.com",
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL: "https://ai-planner-4xv2.onrender.com/api",
 });
 
 export default API;
